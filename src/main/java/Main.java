c5e704b94ea35dc760c4e191aa8dac98dddd48e3
Miramoosh <sanjithr.student@saveetha.ac.in>
2025-11-03 19:49:25 +0530

    feat: added echo commanded

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
             if (command.equals("exit 0")) break;
-            System.out.println(command + ": command not found");
+            String[] commands = command.split(" ");
+            if(command.contains("echo")){
+                for(int i=1;i<commands.length;i++){
+                    System.out.print(commands[i]+" ");
+                }
+                System.out.println();
+            }
+            else {
+                System.out.println(command + ": command not found");
+            }
         }
     }
 }
