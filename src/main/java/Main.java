e886cb564af17cdaa7b9535975457ab590043652
Miramoosh <sanjithr.student@saveetha.ac.in>
2025-11-03 20:13:35 +0530

    feat: added type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,13 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String command = scanner.nextLine();
-            if (command.equals("exit 0")) break;
             String[] commands = command.split(" ");
-            if(command.contains("type echo") || command.contains("type exit") || command.contains("type type")) {
+            if (command.equals("exit 0")) break;
+            else if(command.startsWith("type") && ((command.endsWith("echo")) || (command.endsWith("exit 0")) ||  (command.endsWith("exit")) || (command.endsWith("type")))) {
                 System.out.println(commands[1]+" is a shell builtin");
+            }
+            else if(command.startsWith("type") && ((!command.endsWith("echo")) || (!command.endsWith("exit 0")) ||  (!command.endsWith("exit")) || (!command.endsWith("type")))){
+                System.out.println(commands[1]+" not found");
             }
             else if(command.contains("echo")){
                 for(int i=1;i<commands.length;i++){
