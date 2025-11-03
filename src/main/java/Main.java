b06901f228a849de9caa3c02755ed8f69f3eab85
Miramoosh <sanjithr.student@saveetha.ac.in>
2025-11-03 09:31:34 +0530

    add print messages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
         String commands = sc.nextLine();
-        System.out.println(commands+": command not found");
+        while(true){
+            String command = sc.nextLine();
+            System.out.print("$ ");
+            System.out.println(commands+": command not found");
+        }
     }
 }
